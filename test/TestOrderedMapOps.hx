f28b0ebd92142735c6299d74633899f25cff0bcf
azrafe7 <azrafe7@gmail.com>
2018-09-26 22:47:13 +0200

    add test ops for missing maps

--- test/TestOrderedMapOps.hx
+++ test/TestOrderedMapOps.hx
@@ -6,33 +6,29 @@ class TestOrderedMapOps extends utest.Test {
     super();
   }
 
-  function testOrderedEnumValueMapOps() {
-    new TestOrderedEnumValueMap().setup();
-    var omap = TestOrderedEnumValueMap.buildOrderedMapFrom(TestOrderedEnumValueMap.keys, TestOrderedEnumValueMap.stringValues);
+  function testOrderedStringMapOps() {
+    new TestOrderedStringMap().setup();
+    var omap = TestOrderedStringMap.buildOrderedMapFrom(TestOrderedStringMap.keys, TestOrderedStringMap.stringValues);
     
-    trace(omap);
-    var k = TestOrderedEnumValueMap.keys[2];
+    var k = TestOrderedStringMap.keys[2];
     Assert.isTrue(omap.exists(k));
     omap.remove(k);
     Assert.isFalse(omap.exists(k));
     
-    var v = TestOrderedEnumValueMap.stringValues[2];
+    var v = TestOrderedStringMap.stringValues[2];
     omap.set(k, v);
     Assert.isTrue(omap.exists(k) && omap[k] == v);
     var orderedKeys = TestUtils.iteratorToArray(omap.keys());
     Assert.equals(k, orderedKeys[orderedKeys.length - 1]);
     
-    trace(omap);
     for (k in omap.keys()) omap.remove(k);
     Assert.equals(0, TestUtils.iteratorToArray(omap.iterator()).length);
   }
-
-
+  
   function testOrderedIntMapOps() {
     new TestOrderedIntMap().setup();
     var omap = TestOrderedIntMap.buildOrderedMapFrom(TestOrderedIntMap.keys, TestOrderedIntMap.stringValues);
     
-    trace(omap);
     var k = TestOrderedIntMap.keys[2];
     Assert.isTrue(omap.exists(k));
     omap.remove(k);
@@ -44,13 +40,45 @@ class TestOrderedMapOps extends utest.Test {
     var orderedKeys = TestUtils.iteratorToArray(omap.keys());
     Assert.equals(k, orderedKeys[orderedKeys.length - 1]);
     
-    trace(omap);
-    var it = TestUtils.iteratorToArray(omap.keys());
-    for (k2 in omap.keys()) {
-      
-      omap.remove(k2);
-      trace("  " + k2 + " :" + omap);
-    }
+    for (k in omap.keys()) omap.remove(k);
+    Assert.equals(0, TestUtils.iteratorToArray(omap.iterator()).length);
+  }
+  
+  function testOrderedObjectMapOps() {
+    new TestOrderedObjectMap().setup();
+    var omap = TestOrderedObjectMap.buildOrderedMapFrom(TestOrderedObjectMap.keys, TestOrderedObjectMap.stringValues);
+    
+    var k = TestOrderedObjectMap.keys[2];
+    Assert.isTrue(omap.exists(k));
+    omap.remove(k);
+    Assert.isFalse(omap.exists(k));
+    
+    var v = TestOrderedObjectMap.stringValues[2];
+    omap.set(k, v);
+    Assert.isTrue(omap.exists(k) && omap[k] == v);
+    var orderedKeys = TestUtils.iteratorToArray(omap.keys());
+    Assert.equals(k, orderedKeys[orderedKeys.length - 1]);
+    
+    for (k in omap.keys()) omap.remove(k);
+    Assert.equals(0, TestUtils.iteratorToArray(omap.iterator()).length);
+  }
+  
+  function testOrderedEnumValueMapOps() {
+    new TestOrderedEnumValueMap().setup();
+    var omap = TestOrderedEnumValueMap.buildOrderedMapFrom(TestOrderedEnumValueMap.keys, TestOrderedEnumValueMap.stringValues);
+    
+    var k = TestOrderedEnumValueMap.keys[2];
+    Assert.isTrue(omap.exists(k));
+    omap.remove(k);
+    Assert.isFalse(omap.exists(k));
+    
+    var v = TestOrderedEnumValueMap.stringValues[2];
+    omap.set(k, v);
+    Assert.isTrue(omap.exists(k) && omap[k] == v);
+    var orderedKeys = TestUtils.iteratorToArray(omap.keys());
+    Assert.equals(k, orderedKeys[orderedKeys.length - 1]);
+    
+    for (k in omap.keys()) omap.remove(k);
     Assert.equals(0, TestUtils.iteratorToArray(omap.iterator()).length);
   }
 
