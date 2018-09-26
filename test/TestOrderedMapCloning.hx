19088ac6b5b6c345e0fc14956307e52dddc943c4
azrafe7 <azrafe7@gmail.com>
2018-09-26 20:44:06 +0200

    test OrderedEnumValueMap cloning

--- test/TestOrderedMapCloning.hx
+++ test/TestOrderedMapCloning.hx
@@ -66,4 +66,24 @@ class TestOrderedMapCloning extends utest.Test {
     Assert.same(values, valuesClone);
   }
 
+  function testOrderedEnumValueMapCloning() {
+    new TestOrderedEnumValueMap().setup();
+    var omap = TestOrderedEnumValueMap.buildOrderedMapFrom(TestOrderedEnumValueMap.keys, TestOrderedEnumValueMap.stringValues);
+    var omapClone = omap.copy();
+    
+    Assert.isFalse(omap == omapClone);
+    
+    var keys = TestUtils.iteratorToArray(omap.keys());
+    var keysClone = TestUtils.iteratorToArray(omapClone.keys());
+
+    Assert.isTrue(keys.length == keysClone.length && keys.length > 0);
+    Assert.same(keys, keysClone);
+    
+    var values = TestUtils.iteratorToArray(omap.iterator());
+    var valuesClone = TestUtils.iteratorToArray(omapClone.iterator());
+
+    Assert.isTrue(values.length == valuesClone.length && values.length > 0);
+    Assert.same(values, valuesClone);
+  }
+
 }
