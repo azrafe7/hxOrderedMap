f580130b16157b52d93ea1a2424d4ad8cd57216f
azrafe7 <azrafe7@gmail.com>
2018-09-27 02:13:08 +0200

    [cpp interface inheritance bug] this reproduces it...
    Error: TestOrderedStringMap.cpp
    include\OrderedStringMap.h(60): error C2535: 'void OrderedStringMap_obj::set_41d
    0cb46(Dynamic,Dynamic)': member function already defined or declared
    include\OrderedStringMap.h(50): note: see declaration of 'OrderedStringMap_obj::
    set_41d0cb46'
    
    Error: Build failed

--- src/IOrderedMap.hx
+++ src/IOrderedMap.hx
@@ -1,15 +1,6 @@
 import haxe.Constraints.IMap;
 
-interface IOrderedMap<K, V> /*extends IMap<K, V>*/ {
-  public function get(k:K):Null<V>;
-  public function set(k:K, v:V):Void;
-  public function exists(k:K):Bool;
-  public function remove(k:K):Bool;
-  public function keys():Iterator<K>;
-  public function iterator():Iterator<V>;
-  public function copy():IOrderedMap<K,V>;
-  public function toString():String;
-
+interface IOrderedMap<K, V> extends IMap<K, V> {
   var length(get, null):Int;
   var orderedKeysCopy(get, null):Array<K>;
   function clear():Void;
