ad7274505ebceb893ecc45a107f071aa0fe4c1ed
azrafe7 <azrafe7@gmail.com>
2018-10-02 00:16:54 +0200

    [cpp] better interface workaround?

--- src/IOrderedMap.hx
+++ src/IOrderedMap.hx
@@ -1,6 +1,7 @@
 import haxe.Constraints.IMap;
 
-interface IOrderedMap<K, V> /*extends IMap<K, V>*/ {
+interface IOrderedMap<K, V> extends IMap<K, V> {
+#if cpp
   public function get(k:K):Null<V>;
   public function set(k:K, v:V):Void;
   public function exists(k:K):Bool;
@@ -9,6 +10,7 @@ interface IOrderedMap<K, V> /*extends IMap<K, V>*/ {
   public function iterator():Iterator<V>;
   public function copy():IOrderedMap<K,V>;
   public function toString():String;
+#end
 
   var length(get, null):Int;
   var orderedKeysCopy(get, null):Array<K>;
