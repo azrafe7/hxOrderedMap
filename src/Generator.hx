0c7a84e842081b7926fcedefcf28ca9f8e555c43
azrafe7 <azrafe7@gmail.com>
2018-10-02 17:47:36 +0200

    commit Generator before removal

--- src/Generator.hx
+++ src/Generator.hx
@@ -1,3 +1,4 @@
+import haxe.Json;
 import haxe.io.Path;
 #if !sys
 #error "Can be used only on sys targets"
@@ -6,27 +7,53 @@ import sys.io.File;
 
 using StringTools;
 
+typedef Replacer = {
+  var what:String;
+  var with:String;
+}
+
 class Generator {
-  static var templateFile = "./src/Ordered_TYPE_Map.template.hx";
+  static var templateFiles = [
+    "./src/Ordered_TYPE_Map.template.hx",
+    "./test/TestAll.template.hx",
+    "./test/TestOrdered_TYPE_Map.template.hx",
+  ];
+  static var replacers:Array<Replacer> = [];
 
   static function main() {
     var args = Sys.args();
-    trace(args);
 
     if (args.length < 1)
-      throw "You must pass a type to generator. E.g.: Generator Int";
+      throw 'You must pass replacers definitions to the generator. \n  e.g.: Generator "_TYPE_=Object _KEY_={}"';
 
-    generateFor(args[0]);
+    var defs = args[0].split(" ");
+    for (d in defs) {
+      var p = d.split("=");
+      replacers.push({what: p[0], with: p[1]});
+    }
+
+    Sys.println("\nReplacers:");
+    Sys.println(" " + [for (r in replacers) '${r.with} => ${r.what}']);
+    Sys.println("");
+
+    generate(templateFiles, replacers);
   }
 
-  static function generateFor(type:String, outputFolder:String = "") {
-    var outputType = templateFile.replace("_TYPE_", type).replace(".template", "");
-    var outputPath = Path.join([outputFolder, outputType]);
+  static function generate(templateFiles:Array<String>, replacers:Array<Replacer>) {
+    for (tf in templateFiles) {
+      var outputFile = tf.replace(".template", "");
+      for (rep in replacers) {
+        outputFile = outputFile.replace(rep.what, rep.with);
+      }
+      Sys.println("Processing " + tf + " => " + outputFile + " ...");
+      var template = File.getContent(tf);
+      var output = template;
+      for (rep in replacers) {
+        output = output.replace(rep.what, rep.with);
+      }
 
-    var template = File.getContent(templateFile);
-    var output = template.replace("_TYPE_", type);
-
-    File.saveContent(outputPath, output);
-    trace('File generated and saved in $outputPath.');
+      File.saveContent(outputFile, output);
+      Sys.println(' [OK]');
+    }
   }
 }
