7bedf943e3548b7cf7bda1b41c881e97a819d1c5
azrafe7 <azrafe7@gmail.com>
2018-11-04 21:41:08 +0100

    remove test, just a simple main

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -1,7 +1,3 @@
-import utest.Runner;
-import utest.Test;
-import utest.ui.Report;
-import utest.Assert;
 import IOrderedMap;
 import OrderedMap;
 import OrderedStringMap;
@@ -9,19 +5,8 @@ import OrderedStringMap;
 class TestAll {
 
   public static function main() {
-    var target = TestUtils.detectedTarget();
-    var forceExtendIMap = #if force_extend_imap true #else false #end;
-    trace("defines: \n" + TestUtils.getDefines());
-    trace("detected target  : " + target);
-    trace("force_extend_imap: " + forceExtendIMap + "\n");
 
-    utest.UTest.run([
-      new TestOrderedStringMap(),
-      //new TestOrderedMapCloning(),
-      //new TestOrderedMapOps(),
-      //new TestOrderedMapExtraOps(),
-      //new TestInnerMap(),
-      //new TestKeyValueIterator(),
-    ]);
+    var omap = new OrderedMap();
+    omap["1"] = "one";
   }
 }
