86e860a4ed05ecf4140c14f58c204a5ba138f1f8
azrafe7 <azrafe7@gmail.com>
2018-11-04 22:04:02 +0100

    remove comments

--- src/OrderedMap.hx
+++ src/OrderedMap.hx
@@ -1,118 +1,38 @@
 import haxe.Constraints.IMap;
 
-/**
-  OrderedMap allows key to value mapping for arbitrary value types, and many key
-  types.
-
-  This is a multi-type abstract, it is instantiated as one of its
-  specialization types depending on its type parameters.
-
-  An OrderedMap can be instantiated without explicit type parameters. Type inference
-  will then determine the type parameters from the usage.
-
-  OrderedMap is an abstract type, it is not available at runtime.
-**/
 @:multiType(@:followWithAbstracts K)
-@:native("OrderedMap")
 abstract OrderedMap<K, V>(IOrderedMap<K, V>) {
 
-  /**
-    Creates a new OrderedMap.
-
-    This becomes a constructor call to one of the specialization types in
-    the output. The rules for that are as follows:
-
-    1. if K is a `String`, `OrderedStringMap` is used
-    2. if K is an `Int`, `OrderedIntMap` is used
-    3. if K is an `EnumValue`, `OrderedEnumValueMap` is used
-    4. if K is any other class or structure, `OrderedObjectMap` is used
-    5. if K is any other type, it causes a compile-time error
-  **/
   public function new();
 
-  /**
-    Maps `key` to `value`.
-
-    If `key` already has a mapping, the previous value disappears.
-
-    If `key` is null, the result is unspecified.
-  **/
   public inline function set(key:K, value:V):Void
     this.set(key, value);
 
-  /**
-    Returns the current mapping of `key`.
-
-    If no such mapping exists, null is returned.
-
-    Note that a check like `map.get(key) == null` can hold for two reasons:
-
-    1. the map has no mapping for `key`
-    2. the map has a mapping with a value of `null`
-
-    If it is important to distinguish these cases, `exists()` should be
-    used.
-
-    If `key` is null, the result is unspecified.
-  **/
   @:arrayAccess public inline function get(key:K):Null<V>
     return this.get(key);
 
-  /**
-    Returns true if `key` has a mapping, false otherwise.
-
-    If `key` is null, the result is unspecified.
-  **/
   public inline function exists(key:K):Bool
     return this.exists(key);
 
-  /**
-    Removes the mapping of `key` and returns true if such a mapping existed,
-    false otherwise.
-
-    If `key` is null, the result is unspecified.
-  **/
   public inline function remove(key:K):Bool
     return this.remove(key);
 
-  /**
-    Returns an Iterator over the keys of `this` map in order of insertion.
-  **/
   public inline function keys():Iterator<K> {
-  // @formatter:off
-  // see https://github.com/azrafe7/hxOrderedMap/issues/2#issuecomment-435195303
     return (cast this).keys();
-  // @formatter:on
   }
 
-  /**
-    Returns an Iterator over the values of `this` map in order of insertion.
-  **/
   public inline function iterator():Iterator<V> {
     return this.iterator();
   }
 
-  /**
-    Returns an Iterator over the keys and values of `this` Map in order of insertion.
-  **/
   public inline function keyValueIterator():KeyValueIterator<K, V> {
     return this.keyValueIterator();
   }
 
-  /**
-    Returns a shallow copy of `this` map.
-
-    Key-values ordering will be preserved.
-  **/
   public inline function copy():OrderedMap<K, V> {
     return cast this.copy();
   }
 
-  /**
-    Returns a String representation of `this` map.
-
-    The exact representation depends on the platform and key-type.
-  **/
   public inline function toString():String {
     return this.toString();
   }
--- src/OrderedStringMap.hx
+++ src/OrderedStringMap.hx
@@ -1,76 +1,45 @@
-import haxe.Constraints.IMap;
 import haxe.ds.StringMap;
 
 class OrderedStringMap<T> implements IOrderedMap<String, T> {
 
   var map:StringMap<T> = new StringMap();
 
-  /**
-    Creates a new OrderedStringMap.
-  **/
   public function new():Void {}
 
-  /**
-    See `OrderedMap.set`
-  **/
   public function set(key:String, value:T):Void {
     map.set(key, value);
   }
 
-  /**
-    See `OrderedMap.get`
-  **/
   public inline function get(key:String):Null<T> {
     return map.get(key);
   }
 
-  /**
-    See `OrderedMap.exists`
-  **/
   public inline function exists(key:String):Bool {
     return map.exists(key);
   }
 
-  /**
-    See `OrderedMap.remove`
-  **/
   public function remove(key:String):Bool {
     return map.remove(key);
   }
 
-  /**
-    See `OrderedMap.keys`
-  **/
   public inline function keys():Iterator<String> {
     return map.keys();
   }
 
-  /**
-    See `OrderedMap.iterator`
-  **/
   public inline function iterator():Iterator<T> {
     return map.iterator();
   }
 
-  /**
-    See `OrderedMap.keyValueIterator`
-  **/
   public inline function keyValueIterator():KeyValueIterator<String, T> {
     return new haxe.iterators.MapKeyValueIterator(this);
   }
 
-  /**
-    See `OrderedMap.copy`
-  **/
   public function copy():OrderedStringMap<T> {
     var clone = new OrderedStringMap();
     clone.map = map.copy();
     return clone;
   }
 
-  /**
-    See `OrderedMap.toString`
-  **/
   public function toString():String {
     return map.toString();
   }
