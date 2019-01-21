c648e559ec452dc5004b785c586d27ade15f5898
azrafe7 <azrafe7@gmail.com>
2019-01-21 23:27:17 +0100

    rename bench-* to benchmark-*, add benchs for neko and js

--- bench/Bench.hx
+++ bench/Bench.hx
@@ -7,17 +7,26 @@ class Bench {
   static inline var RUN_BENCHMARKS = true;
 
 	static function main() {
+
     /**
       BENCHMARKS
 
       these run after each target's tests (so all dependencies are already set up)
       and only for the targets that can write to the console
     */
+
     var target = TestUtils.detectedTarget();
-    var isBenchable = [EVAL, NEKO, LUA, PHP, CPP, JAVA, CS, PYTHON, HL, /*Node*/JS].indexOf(target) >= 0;
+    var isBenchable = [EVAL, NEKO, LUA, PHP, CPP, JAVA, CS, PYTHON, HL, NODEJS].indexOf(target) >= 0;
+
+  #if !(sys || nodejs)
+    trace("Skipping benchmarks for " + target + " (doesn't support Sys.println())");
+  #else
+
     if (RUN_BENCHMARKS && isBenchable) {
 
       Sys.println('travis_fold:start:bench-${target}');
+      Sys.println("DEFINES: \n" + TestUtils.getDefines());
+      Sys.println("TARGET: " + target + "\n");
 
       var cases = Macro.getCases("cases");
       var printer = new ResultPrinter();
@@ -32,5 +41,6 @@ class Bench {
 
       Sys.println('travis_fold:end:bench-${target}');
     }
+  #end
 	}
 }
--- bench/TravisUtils.hx
+++ bench/TravisUtils.hx
@@ -1,11 +1,17 @@
 class TravisUtils {
 	static public function successMsg(msg:String):Void {
+  #if (sys || nodejs)
 		Sys.println('\x1b[32m' + msg + '\x1b[0m');
+  #end
 	}
 	static public function failMsg(msg:String):Void {
+  #if (sys || nodejs)
 		Sys.println('\x1b[31m' + msg + '\x1b[0m');
+  #end
 	}
 	static public function infoMsg(msg:String):Void {
+  #if (sys || nodejs)
 		Sys.println('\x1b[36m' + msg + '\x1b[0m');
+  #end
 	}
 }
